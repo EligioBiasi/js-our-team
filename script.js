8209ef7e8d68e0430395375c7242896128118321
Eligio Biasi <ex_98@live.it>
2023-05-29 15:39:07 +0200

    milestone1 added

--- script.js
+++ script.js
@@ -11,42 +11,52 @@
 // Creare l’array di oggetti con le informazioni fornite.
 
 const teamMembers = [
-    firstMember ={
+    {
         nome : 'Wayne Barnett',
         ruolo : 'Founder & CEO',
         foto: 'wayne-barnett-founder-ceo.jpg',
     },
-    secondMember ={
+    {
         nome : 'Angela Caroll',
         ruolo : 'Chief Editor',
         foto: 'angela-caroll-chief-editor.jpg',
     },
-    thirdMember ={
+    {
         nome : 'Walter Gordon',
         ruolo : 'Office Manager',
         foto: 'walter-gordon-office-manager.jpg',
     },
-    fourthMember ={
+    {
         nome : 'Angela Lopez',
         ruolo : 'Social Media Manager',
         foto: 'angela-lopez-social-media-manager.jpg',
     },
-    fifthMember ={
+    {
         nome : 'Scott Estrada',
         ruolo : 'Developer',
         foto: 'scott-estrada-developer.jpg',
     }, 
-    sixthMember ={
+    {
         nome : 'Barbara Ramos',
         ruolo : 'Graphic Designer',
         foto: 'barbara-ramos-graphic-designer.jpg',
     },
 ]
 
-
-
-
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for(let i = 0; i<teamMembers.length; i++){
+    
+    const singleMember = teamMembers[i];
+
+    for(const chiave in singleMember){
+        console.log(chiave, ': ', singleMember[chiave]);
+    }
+    console.log('*****');
+
+}
+
+
+
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhers
