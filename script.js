041713c41096741b8666d0789ebb6c41a4fe3819
Eligio Biasi <ex_98@live.it>
2023-05-29 15:10:47 +0200

    init

---
+++ script.js
@@ -0,0 +1,52 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto
+// Wayne Barnett     Founder & CEO      wayne-barnett-founder-ceo.jpg
+// Angela Caroll     Chief Editor      angela-caroll-chief-editor.jpg
+// Walter Gordon     Office Manager    walter-gordon-office-manager.jpg
+// Angela Lopez      Social Media Manager  angela-lopez-social-media-manager.jpg
+// Scott Estrada     Developer         scott-estrada-developer.jpg
+// Barbara Ramos     Graphic Designer  barbara-ramos-graphic-designer.jpg
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const teamMembers = [
+    firstMember ={
+        nome : 'Wayne Barnett',
+        ruolo : 'Founder & CEO',
+        foto: 'wayne-barnett-founder-ceo.jpg',
+    },
+    secondMember ={
+        nome : 'Angela Caroll',
+        ruolo : 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
+    },
+    thirdMember ={
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager',
+        foto: 'walter-gordon-office-manager.jpg',
+    },
+    fourthMember ={
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+    fifthMember ={
+        nome : 'Scott Estrada',
+        ruolo : 'Developer',
+        foto: 'scott-estrada-developer.jpg',
+    }, 
+    sixthMember ={
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
+
+
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhers
